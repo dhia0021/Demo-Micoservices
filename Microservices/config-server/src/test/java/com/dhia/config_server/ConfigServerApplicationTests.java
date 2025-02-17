38fc9bef4f751cdee73b3ccd5d8ffe3104373220
dhia0021 <mohameddhia.bendkhil@ensi-uma.tn>
2025-02-17 15:25:11 +0100

    demo microservices

---
+++ Microservices/config-server/src/test/java/com/dhia/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhia.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Microservices/microservice1/src/main/java/com/dhia/microservice1/controller.java
@@ -0,0 +1,18 @@
+package com.dhia.microservice1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/microservice1")
+@RequiredArgsConstructor
+public class controller {
+
+    @GetMapping
+    public ResponseEntity<String> méthode() {
+        return ResponseEntity.ok("microservice1");
+    }
+}
